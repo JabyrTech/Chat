058679935957b13a8cf2dae2a1a71c95e0c613bf
Jabir Nasir Argungu <137582122+JabyrTech@users.noreply.github.com>
2025-08-30 08:44:34 +0100

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,5 @@
+from run import app  # replace 'yourapp' with the filename of your Flask app (without .py)
+
+# Vercel looks for "app" by default
+# If your main Flask app is in app.py, then do:
+# from app import app
